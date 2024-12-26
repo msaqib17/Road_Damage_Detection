cef2defdcddbb1138ce0695022c0c274987da5a9
Muhammad Saqib <saq001@whistle4-ep.nexus.csiro.au>
2024-12-27 00:27:55 +1100

    CSPNext with Attention4D Blocks

--- mmdet/models/necks/cspnext_pafpn.py
+++ mmdet/models/necks/cspnext_pafpn.py
@@ -1,16 +1,127 @@
+
 # Copyright (c) OpenMMLab. All rights reserved.
 import math
 from typing import Sequence, Tuple
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
 from mmcv.cnn import ConvModule, DepthwiseSeparableConvModule
 from mmengine.model import BaseModule
 from torch import Tensor
 
+from typing import Dict
+import itertools
 from mmdet.registry import MODELS
 from mmdet.utils import ConfigType, OptMultiConfig
 from ..layers import CSPLayer
+
+
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from timm.models.layers import DropPath, trunc_normal_
+from timm.models.registry import register_model
+from timm.models.layers.helpers import to_2tuple
+from torch.nn.utils import clip_grad_norm_
+
+##########################################################################
+
+class Attention4D(torch.nn.Module):
+    def __init__(self, dim=384, key_dim=32, num_heads=8, attn_ratio=4, resolution=7, act_layer=nn.ReLU, stride=None):
+        super().__init__()
+        self.num_heads = num_heads
+        self.scale = key_dim ** -0.5
+        self.key_dim = key_dim
+        self.nh_kd = nh_kd = key_dim * num_heads
+
+        if stride is not None:
+            self.resolution = math.ceil(resolution / stride)
+            self.stride_conv = nn.Sequential(nn.Conv2d(dim, dim, kernel_size=3, stride=stride, padding=1, groups=dim),
+                                             nn.BatchNorm2d(dim), )
+            self.upsample = nn.Upsample(scale_factor=stride, mode='bilinear')
+        else:
+            self.resolution = resolution
+            self.stride_conv = None
+            self.upsample = None
+
+        self.N = self.resolution ** 2
+        self.N2 = self.N
+        self.d = int(attn_ratio * key_dim)
+        self.dh = int(attn_ratio * key_dim) * num_heads
+        self.attn_ratio = attn_ratio
+        h = self.dh + nh_kd * 2
+        self.q = nn.Sequential(nn.Conv2d(dim, self.num_heads * self.key_dim, 1),
+                               nn.BatchNorm2d(self.num_heads * self.key_dim), )
+        self.k = nn.Sequential(nn.Conv2d(dim, self.num_heads * self.key_dim, 1),
+                               nn.BatchNorm2d(self.num_heads * self.key_dim), )
+        self.v = nn.Sequential(nn.Conv2d(dim, self.num_heads * self.d, 1),
+                               nn.BatchNorm2d(self.num_heads * self.d),
+                               )
+        self.v_local = nn.Sequential(nn.Conv2d(self.num_heads * self.d, self.num_heads * self.d,
+                                               kernel_size=3, stride=1, padding=1, groups=self.num_heads * self.d),
+                                     nn.BatchNorm2d(self.num_heads * self.d), )
+        self.talking_head1 = nn.Conv2d(self.num_heads, self.num_heads, kernel_size=1, stride=1, padding=0)
+        self.talking_head2 = nn.Conv2d(self.num_heads, self.num_heads, kernel_size=1, stride=1, padding=0)
+
+        self.proj = nn.Sequential(act_layer(),
+                                  nn.Conv2d(self.dh, dim, 1),
+                                  nn.BatchNorm2d(dim), )
+
+        points = list(itertools.product(range(self.resolution), range(self.resolution)))
+        N = len(points)
+        attention_offsets = {}
+        idxs = []
+        for p1 in points:
+            for p2 in points:
+                offset = (abs(p1[0] - p2[0]), abs(p1[1] - p2[1]))
+                if offset not in attention_offsets:
+                    attention_offsets[offset] = len(attention_offsets)
+                idxs.append(attention_offsets[offset])
+        self.attention_biases = torch.nn.Parameter(
+            torch.zeros(num_heads, len(attention_offsets)))
+        self.register_buffer('attention_bias_idxs',
+                             torch.LongTensor(idxs).view(N, N))
+
+    @torch.no_grad()
+    def train(self, mode=True):
+        super().train(mode)
+        if mode and hasattr(self, 'ab'):
+            del self.ab
+        else:
+            self.ab = self.attention_biases[:, self.attention_bias_idxs]
+
+    def forward(self, x):  # x (B,N,C)
+        B, C, H, W = x.shape
+        if self.stride_conv is not None:
+            x = self.stride_conv(x)
+
+        q = self.q(x).flatten(2).reshape(B, self.num_heads, -1, self.N).permute(0, 1, 3, 2)
+        k = self.k(x).flatten(2).reshape(B, self.num_heads, -1, self.N).permute(0, 1, 2, 3)
+        v = self.v(x)
+        v_local = self.v_local(v)
+        v = v.flatten(2).reshape(B, self.num_heads, -1, self.N).permute(0, 1, 3, 2)
+
+        attn = (
+                (q @ k) * self.scale
+                +
+                (self.attention_biases[:, self.attention_bias_idxs]
+                 if self.training else self.ab)
+        )
+        # attn = (q @ k) * self.scale
+        attn = self.talking_head1(attn)
+        attn = attn.softmax(dim=-1)
+        attn = self.talking_head2(attn)
+
+        x = (attn @ v)
+
+        out = x.transpose(2, 3).reshape(B, self.dh, self.resolution, self.resolution) + v_local
+   
+
+        if self.upsample is not None:
+            out = self.upsample(out)
+
+        out = self.proj(out)
+        return out
 
 
 @MODELS.register_module()
@@ -67,6 +178,10 @@ class CSPNeXtPAFPN(BaseModule):
         self.upsample = nn.Upsample(**upsample_cfg)
         self.reduce_layers = nn.ModuleList()
         self.top_down_blocks = nn.ModuleList()
+        self.attention4d_layers_top_feat_heigh = nn.ModuleList()  # Attention4D layers for top-down path
+        resolutions = [80, 40, 20]
+
+
         for idx in range(len(in_channels) - 1, 0, -1):
             self.reduce_layers.append(
                 ConvModule(
@@ -88,9 +203,19 @@ class CSPNeXtPAFPN(BaseModule):
                     conv_cfg=conv_cfg,
                     norm_cfg=norm_cfg,
                     act_cfg=act_cfg))
+            self.attention4d_layers_top_feat_heigh.append(Attention4D(
+                dim=in_channels[idx],
+                key_dim=32,
+                num_heads=8,
+                attn_ratio=4,
+                resolution=resolutions[idx],  # Adjust based on your requirements
+                act_layer=nn.ReLU,
+                stride = None
+            ))  
 
         # build bottom-up blocks
         self.downsamples = nn.ModuleList()
+        # self.attention4d_layers_bottom = nn.ModuleList()  # Attention4D layers for bottom-up path
         self.bottom_up_blocks = nn.ModuleList()
         for idx in range(len(in_channels) - 1):
             self.downsamples.append(
@@ -103,6 +228,7 @@ class CSPNeXtPAFPN(BaseModule):
                     conv_cfg=conv_cfg,
                     norm_cfg=norm_cfg,
                     act_cfg=act_cfg))
+            
             self.bottom_up_blocks.append(
                 CSPLayer(
                     in_channels[idx] * 2,
@@ -127,6 +253,9 @@ class CSPNeXtPAFPN(BaseModule):
                     conv_cfg=conv_cfg,
                     norm_cfg=norm_cfg,
                     act_cfg=act_cfg))
+        # self.refinement = nn.ModuleList([ResidualRefinement(out_channels) for _ in range(len(in_channels))])
+
+
 
     def forward(self, inputs: Tuple[Tensor, ...]) -> Tuple[Tensor, ...]:
         """
@@ -140,31 +269,41 @@ class CSPNeXtPAFPN(BaseModule):
 
         # top-down path
         inner_outs = [inputs[-1]]
+
+
         for idx in range(len(self.in_channels) - 1, 0, -1):
             feat_heigh = inner_outs[0]
             feat_low = inputs[idx - 1]
-            feat_heigh = self.reduce_layers[len(self.in_channels) - 1 - idx](
-                feat_heigh)
+            feat_heigh = feat_heigh + self.attention4d_layers_top_feat_heigh[len(self.in_channels) - 1 - idx](feat_heigh)
+
+            feat_heigh = self.reduce_layers[len(self.in_channels) - 1 - idx](feat_heigh)
             inner_outs[0] = feat_heigh
 
             upsample_feat = self.upsample(feat_heigh)
 
             inner_out = self.top_down_blocks[len(self.in_channels) - 1 - idx](
                 torch.cat([upsample_feat, feat_low], 1))
+            
+            
             inner_outs.insert(0, inner_out)
 
         # bottom-up path
         outs = [inner_outs[0]]
+        # print(">>>>>>>>>>>>>>>>>>>>>Bottom up path start here and shape<<<<<<<<<<<<<<<<", inner_outs[0].shape)
         for idx in range(len(self.in_channels) - 1):
             feat_low = outs[-1]
             feat_height = inner_outs[idx + 1]
             downsample_feat = self.downsamples[idx](feat_low)
+
+
             out = self.bottom_up_blocks[idx](
                 torch.cat([downsample_feat, feat_height], 1))
+
             outs.append(out)
 
-        # out convs
         for idx, conv in enumerate(self.out_convs):
             outs[idx] = conv(outs[idx])
-
         return tuple(outs)
+    
+    
+
